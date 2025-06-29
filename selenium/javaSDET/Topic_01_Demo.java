de5581fb4f57a337f50ce5735bbac09ba7aa484f
Bisllly <cuongnguyen3900@gmail.com>
2025-06-29 09:05:46 +0700

    delete topic 01

--- selenium/javaSDET/Topic_01_Demo.java
+++
@@ -1,7 +0,0 @@
-package javaSDET;
-
-public class Topic_01_Demo {
-    public static void main(String[] args) {
-        System.out.println("Hello, this is a demo for Java SDET topic 1.");
-    }
-}
