fde969912b89f418f8a9cac3c1c48811f21abc84
Bisllly <cuongnguyen3900@gmail.com>
2025-06-29 08:56:48 +0700

    add new class

---
+++ selenium/javaSDET/Topic_01_Demo.java
@@ -0,0 +1,7 @@
+package javaSDET;
+
+public class Topic_01_Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello, this is a demo for Java SDET topic 1.");
+    }
+}
